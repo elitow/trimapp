5784c2caa5c0d03ec5d1a12399264fe095a328d6
Eric Litow <elitow@gmail.com>
2015-12-17 17:23:09 -0500

    Defined suvery_email in ApprenticeMailer

--- app/mailers/apprentice_mailer.rb
+++ app/mailers/apprentice_mailer.rb
@@ -1,2 +1,7 @@
 class ApprenticeMailer < ApplicationMailer
+
+  def survey_email(apprentice)
+    @apprentice = apprentice
+    mail(to: elitow@gmail.com, subject: "Apprentice Submission")
+  end
 end
